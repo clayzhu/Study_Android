a46e13cc3363d0ae2ddf41794d32c3465d8f148a
Clay Zhu <clayzhu@163.com>
2019-07-04 14:26:48 +0800

    FrameLayoutDemo2 实现。

--- Demo/2.2.4_FrameLayoutDemo2/app/src/main/java/com/clayzhu/framelayoutdemo2/MainActivity.java
+++ Demo/2.2.4_FrameLayoutDemo2/app/src/main/java/com/clayzhu/framelayoutdemo2/MainActivity.java
@@ -3,6 +3,9 @@ package com.clayzhu.framelayoutdemo2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FrameLayout frame = findViewById(R.id.mylayout);
+        final MeziView mezi = new MeziView(MainActivity.this);
+
+        //为我们的萌妹子添加触摸事件监听器
+        mezi.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                //设置妹子显示的位置
+                mezi.bitmapX = motionEvent.getX() - 150;
+                mezi.bitmapY = motionEvent.getY() - 150;
+                //调用重绘方法
+                mezi.invalidate();
+                return true;
+            }
+        });
+        frame.addView(mezi);
     }
 }
