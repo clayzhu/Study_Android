2f02bfe40d54b125a27356fd9a655b8637a0d2af
Clay Zhu <clayzhu@163.com>
2019-07-09 21:01:03 +0800

    2.3.7_ProgressBarDemo 实现。

--- Demo/2.3.7_ProgressBarDemo/app/src/main/java/com/clayzhu/progressbardemo/MainActivity.java
+++ Demo/2.3.7_ProgressBarDemo/app/src/main/java/com/clayzhu/progressbardemo/MainActivity.java
@@ -2,13 +2,25 @@ package com.clayzhu.progressbardemo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+    private ImageView img_pgbar;
+    private AnimationDrawable ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        img_pgbar = findViewById(R.id.img_pgbar);
+        ad = (AnimationDrawable) img_pgbar.getDrawable();
+        img_pgbar.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                ad.start();
+            }
+        }, 100);
     }
 }
