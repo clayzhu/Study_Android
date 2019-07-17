b2ff2d36c1fdea4b62067e75eb0b8195bc594a67
Clay Zhu <clayzhu@163.com>
2019-07-17 23:30:50 +0800

    添加 Data Model、ListView.

---
+++ Demo/2.4.9_ListViewDemo2/app/src/main/java/com/clayzhu/listviewdemo2/Data.java
@@ -0,0 +1,28 @@
+package com.clayzhu.listviewdemo2;
+
+public class Data {
+
+    private int imgId;
+    private String content;
+
+    public Data(int imgId, String content) {
+        this.imgId = imgId;
+        this.content = content;
+    }
+
+    public int getImgId() {
+        return imgId;
+    }
+
+    public void setImgId(int imgId) {
+        this.imgId = imgId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
