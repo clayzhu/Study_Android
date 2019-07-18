64d2e4be83f3e44f62e7e0a03ace4af2fbaf7c69
Clay Zhu <clayzhu@163.com>
2019-07-18 21:04:24 +0800

    ListView 无数据时显示 TextView.

--- Demo/2.4.9_ListViewDemo2/app/src/main/java/com/clayzhu/listviewdemo2/MainActivity.java
+++ Demo/2.4.9_ListViewDemo2/app/src/main/java/com/clayzhu/listviewdemo2/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.os.Bundle;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.LinkedList;
 import java.util.List;
@@ -15,6 +16,10 @@ public class MainActivity extends AppCompatActivity {
     private MyAdapter mAdapter = null;
     private List<Data> mData = null;
     private Context mContext = null;
+    /**
+     * ListView 无数据时显示
+     */
+    private TextView txt_empty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,5 +35,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void bindViews() {
         list_one = findViewById(R.id.list_one);
+
+        txt_empty = findViewById(R.id.txt_empty);
+        txt_empty.setText("暂无数据～");
+        list_one.setEmptyView(txt_empty);
     }
 }
