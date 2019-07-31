21f4e8387cf8f6d89c80cfae1ad40e78dfe0c4c6
Clay Zhu <clayzhu@163.com>
2019-07-31 17:31:40 +0800

    ViewHolder 类的升级改造：绑定 ViewHolder 与 item；根据 id 获取集合中保存的控件；公共方法等。

--- Demo/2.5.0_ListViewDemo3/app/src/main/java/com/clayzhu/listviewdemo3/MyAdapter.java
+++ Demo/2.5.0_ListViewDemo3/app/src/main/java/com/clayzhu/listviewdemo3/MyAdapter.java
@@ -107,5 +107,90 @@ public class MyAdapter<T> extends BaseAdapter {
 
         ImageView img_icon;
         TextView txt_content;
+
+        //绑定ViewHolder与item
+        public static ViewHolder bind(Context context, View convertView, ViewGroup parent, int layoutRes, int postion) {
+            ViewHolder holder;
+            if (convertView == null) {
+                holder = new ViewHolder(context, parent, layoutRes);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+                holder.item = convertView;
+            }
+            holder.position = postion;
+            return holder;
+        }
+
+        // 根据id获取集合中保存的控件
+        public <T extends View> T getView(int id) {
+            T t = (T) mViews.get(id);
+            if (t == null) {
+                t = (T) item.findViewById(id);
+                mViews.put(id, t);
+            }
+            return t;
+        }
+
+        /**
+         * 获取当前条目
+         */
+        public View getItemView() {
+            return item;
+        }
+
+        /**
+         * 获取条目位置
+         */
+        public int getItemPosition() {
+            return position;
+        }
+
+        /**
+         * 设置文字
+         */
+        public ViewHolder setText(int id, CharSequence text) {
+            View view = getView(id);
+            if (view instanceof TextView) {
+                ((TextView) view).setText(text);
+            }
+            return this;
+        }
+
+        /**
+         * 设置图片
+         */
+        public ViewHolder setImageResource(int id, int drawableRes) {
+            View view = getView(id);
+            if (view instanceof ImageView) {
+                ((ImageView) view).setImageResource(drawableRes);
+            } else {
+                view.setBackgroundResource(drawableRes);
+            }
+            return this;
+        }
+
+        /**
+         * 设置点击监听
+         */
+        public ViewHolder setOnClickListener(int id, View.OnClickListener listener) {
+            getView(id).setOnClickListener(listener);
+            return this;
+        }
+
+        /**
+         * 设置可见
+         */
+        public ViewHolder setVisibility(int id, int visible) {
+            getView(id).setVisibility(visible);
+            return this;
+        }
+
+        /**
+         * 设置标签
+         */
+        public ViewHolder setTag(int id, Object obj) {
+            getView(id).setTag(obj);
+            return this;
+        }
     }
 }
