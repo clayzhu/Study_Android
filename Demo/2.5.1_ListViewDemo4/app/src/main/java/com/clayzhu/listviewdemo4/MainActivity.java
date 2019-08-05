b343d210b7364fdefc7bac4d29974c99df4b8753
Clay Zhu <clayzhu@163.com>
2019-08-05 20:07:45 +0800

    2.5.1_ListViewDemo4 实现 ListView item 多布局。

--- Demo/2.5.1_ListViewDemo4/app/src/main/java/com/clayzhu/listviewdemo4/MainActivity.java
+++ Demo/2.5.1_ListViewDemo4/app/src/main/java/com/clayzhu/listviewdemo4/MainActivity.java
@@ -2,12 +2,39 @@ package com.clayzhu.listviewdemo4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int TYPE_BOOK = 0;
+    private static final int TYPE_APP = 1;
+
+    private ListView list_content;
+    private ArrayList<Object> mData = null;
+    private MultiLayoutAdapter mAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //数据准备
+        mData = new ArrayList<>();
+        for (int i = 0; i < 20; i ++) {
+            switch ((int) (Math.random() * 2)) {
+                case TYPE_APP:
+                    mData.add(new App(R.mipmap.iv_icon_baidu, "百度"));
+                    break;
+                case TYPE_BOOK:
+                    mData.add(new Book("《第一行代码Android》", "郭霖"));
+                    break;
+            }
+        }
+
+        list_content = findViewById(R.id.list_content);
+        mAdapter = new MultiLayoutAdapter(MainActivity.this, mData);
+        list_content.setAdapter(mAdapter);
     }
 }
--- Demo/2.5.1_ListViewDemo4/app/src/main/java/com/clayzhu/listviewdemo4/MultiLayoutAdapter.java
+++ Demo/2.5.1_ListViewDemo4/app/src/main/java/com/clayzhu/listviewdemo4/MultiLayoutAdapter.java
@@ -1,9 +1,12 @@
 package com.clayzhu.listviewdemo4;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -15,6 +18,11 @@ public class MultiLayoutAdapter extends BaseAdapter {
 
     private Context mContext;
     private ArrayList<Object> mData = null;
+
+    public MultiLayoutAdapter(Context mContext, ArrayList<Object> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
 
     @Override
     public int getCount() {
@@ -28,11 +36,85 @@ public class MultiLayoutAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
-        return 0;
+        return position;
+    }
+
+    //多布局的核心，通过这个判断类别
+    @Override
+    public int getItemViewType(int position) {
+        if (mData.get(position) instanceof App) {
+            return TYPE_APP;
+        } else if (mData.get(position) instanceof Book) {
+            return TYPE_BOOK;
+        } else {
+            return super.getItemViewType(position);
+        }
+    }
+
+    //类别数目
+    @Override
+    public int getViewTypeCount() {
+        return 2;
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+        int type = getItemViewType(position);
+        ViewHolder1 holder1 = null;
+        ViewHolder2 holder2 = null;
+        if (convertView == null) {
+            switch (type) {
+                case TYPE_APP:
+                    holder1 = new ViewHolder1();
+                    convertView = LayoutInflater.from(mContext).inflate(R.layout.item_one, parent, false);
+                    holder1.img_aicon = convertView.findViewById(R.id.img_aicon);
+                    holder1.txt_aname = convertView.findViewById(R.id.txt_aname);
+                    convertView.setTag(R.id.Tag_App, holder1);
+                    break;
+                case TYPE_BOOK:
+                    holder2 = new ViewHolder2();
+                    convertView = LayoutInflater.from(mContext).inflate(R.layout.item_two, parent, false);
+                    holder2.txt_bname = convertView.findViewById(R.id.txt_bname);
+                    holder2.txt_bauthor = convertView.findViewById(R.id.txt_bauthor);
+                    convertView.setTag(R.id.Tag_Book, holder2);
+                    break;
+            }
+        } else {
+            switch (type) {
+                case TYPE_APP:
+                    holder1 = (ViewHolder1) convertView.getTag(R.id.Tag_App);
+                    break;
+                case TYPE_BOOK:
+                    holder2 = (ViewHolder2) convertView.getTag(R.id.Tag_Book);
+                    break;
+            }
+        }
+
+        Object obj = mData.get(position);
+        //设置下控件的值
+        switch (type) {
+            case TYPE_APP:
+                App app = (App) obj;
+                holder1.img_aicon.setImageResource(app.getaIcon());
+                holder1.txt_aname.setText(app.getaName());
+                break;
+            case TYPE_BOOK:
+                Book book = (Book) obj;
+                holder2.txt_bname.setText(book.getbName());
+                holder2.txt_bauthor.setText(book.getbAuthor());
+                break;
+        }
+        return convertView;
+    }
+
+    //两个不同的ViewHolder
+    private static class ViewHolder1 {
+        ImageView img_aicon;
+        TextView txt_aname;
+    }
+
+    private static class ViewHolder2 {
+        TextView txt_bname;
+        TextView txt_bauthor;
     }
 }
