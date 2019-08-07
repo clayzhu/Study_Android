237722d204c41e083652b55c615142ff1e9ef17a
Clay Zhu <clayzhu@163.com>
2019-08-07 23:49:55 +0800

    MainActivity 布局和实现。

--- Demo/2.5.3_SpinnerDemo/app/src/main/java/com/clayzhu/spinnerdemo/MainActivity.java
+++ Demo/2.5.3_SpinnerDemo/app/src/main/java/com/clayzhu/spinnerdemo/MainActivity.java
@@ -1,13 +1,87 @@
 package com.clayzhu.spinnerdemo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    private Spinner spin_one;
+    private Spinner spin_two;
+
+    private Context mContext;
+
+    //判断是否为刚进去时触发onItemSelected的标志
+    private boolean one_selected = false;
+    private boolean two_selected = false;
+
+    private ArrayList<Hero> mData = null;
+    private MyAdapter<Hero> myAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+        mData = new ArrayList<>();
+        bindViews();
+    }
+
+    private void bindViews() {
+        spin_one = findViewById(R.id.spin_one);
+        spin_two = findViewById(R.id.spin_two);
+
+        mData.add(new Hero(R.mipmap.iv_lol_icon1, "迅捷斥候：提莫（Teemo）"));
+        mData.add(new Hero(R.mipmap.iv_lol_icon2, "诺克萨斯之手：德莱厄斯（Darius）"));
+        mData.add(new Hero(R.mipmap.iv_lol_icon3, "无极剑圣：易（Yi）"));
+        mData.add(new Hero(R.mipmap.iv_lol_icon4,"德莱厄斯：德莱文（Draven）"));
+        mData.add(new Hero(R.mipmap.iv_lol_icon5,"德邦总管：赵信（XinZhao）"));
+        mData.add(new Hero(R.mipmap.iv_lol_icon6,"狂战士：奥拉夫（Olaf）"));
+
+        myAdapter = new MyAdapter<Hero>(mData, R.layout.item_spin_hero) {
+            @Override
+            public void bindView(ViewHolder holder, Hero obj) {
+                holder.setImageResource(R.id.img_icon, obj.gethIcon());
+                holder.setText(R.id.txt_name, obj.gethName());
+            }
+        };
+        spin_two.setAdapter(myAdapter);
+
+        spin_one.setOnItemSelectedListener(this);
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        switch (parent.getId()) {
+            case R.id.spin_one:
+                if (one_selected) {
+                    Toast.makeText(mContext, "您的分段是～：" + parent.getItemAtPosition(position).toString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    one_selected = true;
+                }
+                break;
+            case R.id.spin_two:
+                if (two_selected) {
+                    TextView txt_name = view.findViewById(R.id.txt_name);
+                    Toast.makeText(mContext, "您选择的英雄是～：" + txt_name.getText().toString(), Toast.LENGTH_SHORT).show();
+                } else {
+                    two_selected = true;
+                }
+                break;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
