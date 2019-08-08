d71c64b9700076d7eab5576495ff8cc402f6b1ee
Clay Zhu <clayzhu@163.com>
2019-08-09 01:26:52 +0800

    2.5.4_AutoCompleteTextViewDemo 实现。

--- Demo/2.5.4_AutoCompleteTextViewDemo/app/src/main/java/com/clayzhu/autocompletetextviewdemo/MainActivity.java
+++ Demo/2.5.4_AutoCompleteTextViewDemo/app/src/main/java/com/clayzhu/autocompletetextviewdemo/MainActivity.java
@@ -2,12 +2,32 @@ package com.clayzhu.autocompletetextviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.MultiAutoCompleteTextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView atv_content;
+    private MultiAutoCompleteTextView matv_content;
+
+    private static final String[] data = new String[] {
+            "小猪猪", "小狗狗", "小鸡鸡", "小猫猫", "小咪咪"
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        atv_content = findViewById(R.id.atv_content);
+        matv_content = findViewById(R.id.matv_content);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_dropdown_item_1line, data);
+        atv_content.setAdapter(adapter);
+
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_dropdown_item_1line, data);
+        matv_content.setAdapter(adapter);
+        matv_content.setTokenizer(new MultiAutoCompleteTextView.CommaTokenizer());
     }
 }
