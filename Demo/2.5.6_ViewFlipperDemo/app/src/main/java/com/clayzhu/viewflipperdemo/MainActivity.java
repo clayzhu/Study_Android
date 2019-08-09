c28f976d82a2f08965c1b2f7a9079c6e08bbb778
Clay Zhu <clayzhu@163.com>
2019-08-09 23:55:11 +0800

    2.5.6_ViewFlipperDemo 自动切换实现。

--- Demo/2.5.6_ViewFlipperDemo/app/src/main/java/com/clayzhu/viewflipperdemo/MainActivity.java
+++ Demo/2.5.6_ViewFlipperDemo/app/src/main/java/com/clayzhu/viewflipperdemo/MainActivity.java
@@ -2,12 +2,18 @@ package com.clayzhu.viewflipperdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ViewFlipper vflp_help;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        vflp_help = findViewById(R.id.vflp_help);
+        vflp_help.startFlipping();
     }
 }
