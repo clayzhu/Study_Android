213f0d76784a23be6ef35386cf10002c916fcddc
Clay Zhu <clayzhu@163.com>
2019-08-09 22:43:34 +0800

    2.5.5_ExpandableListViewDemo Adapter、MainActivity 实现。

--- Demo/2.5.5_ExpandableListViewDemo/app/src/main/java/com/clayzhu/expandablelistviewdemo/MainActivity.java
+++ Demo/2.5.5_ExpandableListViewDemo/app/src/main/java/com/clayzhu/expandablelistviewdemo/MainActivity.java
@@ -1,13 +1,73 @@
 package com.clayzhu.expandablelistviewdemo;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ExpandableListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Group> gData = null;
+    private ArrayList<ArrayList<Item>> iData = null;
+    private ArrayList<Item> lData = null;
+
+    private Context mContext;
+    private ExpandableListView exlist_lol;
+    private MyBaseExpandableListAdapter myAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+        exlist_lol = findViewById(R.id.exlist_lol);
+
+        //数据准备
+        gData = new ArrayList<>();
+        iData = new ArrayList<>();
+        gData.add(new Group("AD"));
+        gData.add(new Group("AP"));
+        gData.add(new Group("TANK"));
+
+        //AD组
+        lData = new ArrayList<Item>();
+        lData.add(new Item(R.mipmap.iv_lol_icon3,"剑圣"));
+        lData.add(new Item(R.mipmap.iv_lol_icon4,"德莱文"));
+        lData.add(new Item(R.mipmap.iv_lol_icon13,"男枪"));
+        lData.add(new Item(R.mipmap.iv_lol_icon14,"韦鲁斯"));
+        iData.add(lData);
+        //AP组
+        lData = new ArrayList<Item>();
+        lData.add(new Item(R.mipmap.iv_lol_icon1, "提莫"));
+        lData.add(new Item(R.mipmap.iv_lol_icon7, "安妮"));
+        lData.add(new Item(R.mipmap.iv_lol_icon8, "天使"));
+        lData.add(new Item(R.mipmap.iv_lol_icon9, "泽拉斯"));
+        lData.add(new Item(R.mipmap.iv_lol_icon11, "狐狸"));
+        iData.add(lData);
+        //TANK组
+        lData = new ArrayList<Item>();
+        lData.add(new Item(R.mipmap.iv_lol_icon2, "诺手"));
+        lData.add(new Item(R.mipmap.iv_lol_icon5, "德邦"));
+        lData.add(new Item(R.mipmap.iv_lol_icon6, "奥拉夫"));
+        lData.add(new Item(R.mipmap.iv_lol_icon10, "龙女"));
+        lData.add(new Item(R.mipmap.iv_lol_icon12, "狗熊"));
+        iData.add(lData);
+
+        myAdapter = new MyBaseExpandableListAdapter(gData, iData, mContext);
+        exlist_lol.setAdapter(myAdapter);
+
+        //为列表设置点击事件
+        exlist_lol.setOnChildClickListener(new ExpandableListView.OnChildClickListener() {
+            @Override
+            public boolean onChildClick(ExpandableListView parent, View v, int groupPosition, int childPosition, long id) {
+                Toast.makeText(mContext, "你点击了：" + iData.get(groupPosition).get(childPosition).getiName(), Toast.LENGTH_SHORT).show();
+                return true;
+            }
+        });
     }
 }
