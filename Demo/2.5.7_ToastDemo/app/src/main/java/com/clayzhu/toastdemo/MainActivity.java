b9e9b1b68603b0f975148074e10a3ef4e6ba6262
Clay Zhu <clayzhu@163.com>
2019-08-10 20:41:59 +0800

    2.5.7_ToastDemo 实现自定义。

--- Demo/2.5.7_ToastDemo/app/src/main/java/com/clayzhu/toastdemo/MainActivity.java
+++ Demo/2.5.7_ToastDemo/app/src/main/java/com/clayzhu/toastdemo/MainActivity.java
@@ -1,13 +1,48 @@
 package com.clayzhu.toastdemo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btn_toast;
+    private Context mContext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mContext = MainActivity.this;
+        btn_toast = findViewById(R.id.btn_toast);
+        btn_toast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                midToast("提莫队长，正在送命～", Toast.LENGTH_SHORT);
+            }
+        });
+    }
+
+    private void midToast(String str, int showTime) {
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.view_toast_custom, (ViewGroup) findViewById(R.id.lly_toast));
+        ImageView img_logo = view.findViewById(R.id.img_logo);
+        TextView tv_msg = view.findViewById(R.id.tv_msg);
+        tv_msg.setText(str);
+
+        Toast toast = new Toast(mContext);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration(showTime);
+        toast.setView(view);
+        toast.show();
     }
 }
