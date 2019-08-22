4350fc0915d47db1c12a147b712033d0b7df226f
Clay Zhu <clayzhu@163.com>
2019-08-22 23:38:58 +0800

    OptionMenu 实现。

--- Demo/2.6.2_MenuDemo/app/src/main/java/com/clayzhu/menudemo/MainActivity.java
+++ Demo/2.6.2_MenuDemo/app/src/main/java/com/clayzhu/menudemo/MainActivity.java
@@ -1,13 +1,75 @@
 package com.clayzhu.menudemo;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    //1.定义不同颜色的菜单项的标识:
+    final private int RED = 110;
+    final private int GREEN = 111;
+    final private int BLUE = 112;
+    final private int YELLOW = 113;
+    final private int GRAY= 114;
+    final private int CYAN= 115;
+    final private int BLACK= 116;
+
+    private TextView tv_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_test = findViewById(R.id.tv_test);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menu.add(1, RED, 4, "红色");
+        menu.add(1, GREEN, 2, "绿色");
+        menu.add(1, BLUE, 3, "蓝色");
+        menu.add(1, YELLOW, 1, "黄色");
+        menu.add(1, GRAY, 5, "灰色");
+        menu.add(1, CYAN, 6, "蓝绿色");
+        menu.add(1, BLACK, 7, "黑色");
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id) {
+            case RED:
+                tv_test.setTextColor(Color.RED);
+                break;
+            case GREEN:
+                tv_test.setTextColor(Color.GREEN);
+                break;
+            case BLUE:
+                tv_test.setTextColor(Color.BLUE);
+                break;
+            case YELLOW:
+                tv_test.setTextColor(Color.YELLOW);
+                break;
+            case GRAY:
+                tv_test.setTextColor(Color.GRAY);
+                break;
+            case CYAN:
+                tv_test.setTextColor(Color.CYAN);
+                break;
+            case BLACK:
+                tv_test.setTextColor(Color.BLACK);
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
